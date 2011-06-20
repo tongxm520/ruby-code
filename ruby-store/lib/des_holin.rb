79bbf5ce8edc0e2611a7e908512f74e7cd679d1f
simon <simon@gmail.com>
2011-06-20 14:59:29 +0800

    add ruby-store

---
+++ ruby-store/lib/des_holin.rb
@@ -0,0 +1,48 @@
+require 'openssl'
+require 'base64'
+
+message = %(1. 服务器空间（m0~m8）)
+key = "yourpass"
+#encrypt_key = Digest::SHA1.hexdigest("yourpass")
+encrypt_key = key
+
+cipher = OpenSSL::Cipher::Cipher.new("des-cbc")
+cipher.encrypt # Call this before setting key or iv
+cipher.key = encrypt_key
+#iv = cipher.iv = cipher.random_iv
+ciphertext = cipher.update(message)
+ciphertext << cipher.final
+puts "Encrypted \"#{message}\" with \"#{key}\" to:\n\"#{ciphertext}\"\n"
+puts "--------------------------------------------------------"
+
+
+#============================================================
+
+
+# Base64-encode the ciphertext
+encodedCipherText = Base64.encode64(ciphertext)
+puts "AfterBase64 encodedCipherText is : #{encodedCipherText}"
+puts "--------------------------------------------------------"
+
+# Base64-decode the ciphertext and decrypt it
+decodedCipherText = Base64.decode64(encodedCipherText)
+puts "AfterBase64 decodedCipherText is : #{decodedCipherText}"
+puts "--------------------------------------------------------"
+
+
+#============================================================
+
+
+cipher = OpenSSL::Cipher::Cipher.new("des-cbc")
+cipher.decrypt
+cipher.key = encrypt_key
+#cipher.iv = iv
+plaintext = cipher.update(decodedCipherText)
+plaintext << cipher.final
+
+# Print decrypted plaintext; should match original message
+puts plaintext.class
+puts "Decrypted: \"#{plaintext}\""
+
+
+
---
+++ ruby-store/lib/test_module.rb
@@ -0,0 +1,37 @@
+require "api_tool"
+
+class TestModule
+	include ApiTool
+	load_tool
+
+  def test_pre_process_for_params
+    puts "test_pre_process_for_params"
+    pre_process_for_params
+  end
+
+  def test_compress
+    puts "test_compress"
+    compress
+  end	
+	
+  def msg(*op)
+    hint = "msg: $0"
+    op.size.times do |i|
+      hint.gsub!("$#{i}", op[i])
+    end
+    puts hint
+  end
+end
+
+t = TestModule.new
+#t.pre_process_for_params
+t.compress
+t.test_pre_process_for_params
+t.test_compress
+puts "----------------------"
+t.msg("abc")
+t.msg
+
+
+
+
