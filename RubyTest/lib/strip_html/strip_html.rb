1aeb43c9e82c813b50283b6b7c420c405fec8c99
simon <tong.xm520@gmail.com>
2011-09-08 19:20:55 +0800

    all

--- RubyTest/lib/strip_html/strip_html.rb
+++ RubyTest/lib/strip_html/strip_html.rb
@@ -6,7 +6,7 @@ File.open("#{File.dirname(__FILE__)}/html.txt", "r").each_line do |line|
   end
 end
 
-no_html = html.gsub(/<\/?.*?>/, "")
+no_html = html.gsub(/<\/?.*?>/, "").gsub(/<script.*?>.*<\/script>/, "")
 File.open("#{File.dirname(__FILE__)}/no_html.txt", "w") do |f|
   f.write no_html
 end
